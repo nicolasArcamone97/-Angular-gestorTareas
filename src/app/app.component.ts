e46e85b415ce6b689f7e2b59be911ebab08886d0
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-16 21:44:39 -0300

    creado de template y estilos de page principal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {FormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule,MatCardModule,FormsModule,MatCheckboxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-gestortareas';
+  title = 'Esto es un gestor de tareas';
+
+  
 }
+
+

f149d48b6b5395b2a25516d9a18c40b2d5dd5226
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-22 22:17:22 -0300

    solucion bug

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# AngularGestortareas
+# AngularGestorTareas
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.1.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.2.1.
 
 ## Development server
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {FormsModule} from '@angular/forms';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatButtonModule, MatDividerModule, MatIconModule,MatCardModule,FormsModule,MatCheckboxModule],
+  imports: [RouterOutlet,MatCardModule,MatCheckboxModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Esto es un gestor de tareas';
-
-  
+  title = 'angular-gestorTareas';
 }
-
 

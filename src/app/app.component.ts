3157397c7e4c70aef75278814028ab9687df5b12
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-22 22:37:18 -0300

    agregado de dropdown cerrer sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatCardModule,MatCheckboxModule],
+  imports: [RouterOutlet,MatCardModule,MatCheckboxModule, MatMenuModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

c1c407479abec1d28b895ce04b7468a4b9676f9c
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-29 16:26:22 -0300

    creado de modal con selector de colores a evaluar

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -26,25 +26,25 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.listTarea = [
-      {
-          "titulo": "Desarrollar formulario de registro de usuarios",
-          "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta."
-      },
-      {
-          "titulo": "Implementar autenticación de usuarios",
-          "descripcion": "Configurar un sistema de autenticación que permita a los usuarios iniciar sesión con su correo electrónico y contraseña. Utilizar un token JWT para mantener la sesión activa."
-      },
-      {
-          "titulo": "Crear un sistema de gestión de productos",
-          "descripcion": "Desarrollar un módulo que permita a los administradores agregar, modificar, eliminar y consultar productos en la base de datos. Incluir campos como nombre del producto, descripción, precio y categoría."
-      }
+    //   {
+    //       "titulo": "Desarrollar formulario de registro de usuarios",
+    //       "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta."
+    //   },
+    //   {
+    //       "titulo": "Implementar autenticación de usuarios",
+    //       "descripcion": "Configurar un sistema de autenticación que permita a los usuarios iniciar sesión con su correo electrónico y contraseña. Utilizar un token JWT para mantener la sesión activa."
+    //   },
+    //   {
+    //       "titulo": "Crear un sistema de gestión de productos",
+    //       "descripcion": "Desarrollar un módulo que permita a los administradores agregar, modificar, eliminar y consultar productos en la base de datos. Incluir campos como nombre del producto, descripción, precio y categoría."
+    //   }
   ]
 
   this.listCompletadas = [
-    {
-        "titulo": "Desarrollar formulario de registro de usuarios",
-        "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta."
-    }
+    // {
+    //     "titulo": "Desarrollar formulario de registro de usuarios",
+    //     "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta."
+    // }
   ]
 
   

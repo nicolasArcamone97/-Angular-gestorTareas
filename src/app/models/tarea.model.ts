14b0eeacd2b999cf60019a21c0039cbf28722cee
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-29 22:30:18 -0300

    creado de enums y manejo de colores de tareas

--- src/app/models/tarea.model.ts
+++ src/app/models/tarea.model.ts
@@ -1,4 +1,7 @@
+import { ColorEnum } from "../enums/color.enum";
+
 export interface Tarea {
     titulo: String
     descripcion: String;
+    color: ColorEnum
 }
--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -7,6 +7,7 @@ import { TareaComponent } from "../../components/tarea/tarea.component";
 import { Tarea } from '../../models/tarea.model';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from "../../components/modal/modal.component";
+import { ColorEnum } from '../../enums/color.enum';
 
 @Component({
   selector: 'app-home',
@@ -31,24 +32,19 @@ export class HomeComponent implements OnInit {
     this.listTarea = [
       {
           "titulo": "Desarrollar formulario de registro de usuarios",
-          "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta."
-      },
+          "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta.",
+          "color":ColorEnum.green
+        },
       {
           "titulo": "Implementar autenticación de usuarios",
-          "descripcion": "Configurar un sistema de autenticación que permita a los usuarios iniciar sesión con su correo electrónico y contraseña. Utilizar un token JWT para mantener la sesión activa."
-      },
+          "descripcion": "Configurar un sistema de autenticación que permita a los usuarios iniciar sesión con su correo electrónico y contraseña. Utilizar un token JWT para mantener la sesión activa.",
+          "color":ColorEnum.yellow
+        },
       {
           "titulo": "Crear un sistema de gestión de productos",
-          "descripcion": "Desarrollar un módulo que permita a los administradores agregar, modificar, eliminar y consultar productos en la base de datos. Incluir campos como nombre del producto, descripción, precio y categoría."
-      },
-      {
-          "titulo": "Implementar autenticación de usuarios",
-          "descripcion": "Configurar un sistema de autenticación que permita a los usuarios iniciar sesión con su correo electrónico y contraseña. Utilizar un token JWT para mantener la sesión activa."
-      },
-      {
-          "titulo": "Crear un sistema de gestión de productos",
-          "descripcion": "Desarrollar un módulo que permita a los administradores agregar, modificar, eliminar y consultar productos en la base de datos. Incluir campos como nombre del producto, descripción, precio y categoría."
-      }
+          "descripcion": "Desarrollar un módulo que permita a los administradores agregar, modificar, eliminar y consultar productos en la base de datos. Incluir campos como nombre del producto, descripción, precio y categoría.",
+          "color":ColorEnum.blue
+        }
   ]
 
   this.listCompletadas = [

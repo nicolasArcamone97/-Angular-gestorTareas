0ff11f2d97aa7ec875d2d504566158caa74c2498
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-08-30 00:31:40 -0300

    conexion con backend

--- src/app/page/home/home.component.ts
+++ src/app/page/home/home.component.ts
@@ -7,12 +7,16 @@ import { TareaComponent } from "../../components/tarea/tarea.component";
 import { Tarea } from '../../models/tarea.model';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from "../../components/modal/modal.component";
-import { ColorEnum } from '../../enums/color.enum';
+import { TareaService } from '../../services/tarea.service';
+
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [RouterOutlet, MatCardModule, MatCheckboxModule, MatMenuModule, TareaComponent, CommonModule, ModalComponent],
+  providers:[TareaService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -26,36 +30,19 @@ export class HomeComponent implements OnInit {
 
   colorRecibido?: String;
 
-  constructor() {}
+  constructor(private _tareaService:TareaService) {}
 
   ngOnInit(): void {
-    this.listTarea = [
-      {
-          "titulo": "Desarrollar formulario de registro de usuarios",
-          "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta.",
-          "color":ColorEnum.green
-        },
-      {
-          "titulo": "Implementar autenticación de usuarios",
-          "descripcion": "Configurar un sistema de autenticación que permita a los usuarios iniciar sesión con su correo electrónico y contraseña. Utilizar un token JWT para mantener la sesión activa.",
-          "color":ColorEnum.yellow
-        },
-      {
-          "titulo": "Crear un sistema de gestión de productos",
-          "descripcion": "Desarrollar un módulo que permita a los administradores agregar, modificar, eliminar y consultar productos en la base de datos. Incluir campos como nombre del producto, descripción, precio y categoría.",
-          "color":ColorEnum.blue
-        }
-  ]
-
-  this.listCompletadas = [
-    // {
-    //     "titulo": "Desarrollar formulario de registro de usuarios",
-    //     "descripcion": "Crear un formulario de registro que permita a los usuarios ingresar sus datos personales como nombre, correo electrónico y contraseña. Validar los campos para asegurar que la información ingresada sea correcta."
-    // }
-  ]
-
-  
+    this.obtenerTareas()
   }
+
+
+  obtenerTareas(){
+    this._tareaService.obtenerTareas().subscribe((data:Tarea[]) => {
+      this.listTarea = data
+    })
+  }
+
 
 
   recibirTarea(tarea:Tarea){
--- src/app/services/tarea.service.ts
+++ src/app/services/tarea.service.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { Tarea } from '../models/tarea.model';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
+  private baseUrl = 'http://localhost:3000/tarea'
  
-  constructor() { 
-  
+  constructor(private http:HttpClient) { }
+
+  public obtenerTareas(): Observable<Tarea[]>{
+    return this.http.get<Tarea[]>(this.baseUrl)
   }
 
+  public obtenerTarea(idTarea:number): Observable<Tarea>{
+    return this.http.get<Tarea>(`${this.baseUrl}/${idTarea}`)
+  }
+
+  public crearTarea(tarea:Tarea):Observable<void>{
+    return this.http.post<void>(`${this.baseUrl}`, tarea)
+  }
 
 
 

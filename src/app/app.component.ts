efa8942ea3fde284a2e5e079019aa8df3a3cb1b0
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2024-09-04 20:09:45 -0300

    creado de template, estilos de register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MatCardModule } from '@angular/material/card';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatMenuModule } from '@angular/material/menu';
-import { HomeComponent } from "./page/home/home.component";
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { TareaComponent } from './components/tarea/tarea.component';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatCardModule, MatCheckboxModule, MatMenuModule, HomeComponent,SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-gestorTareas';
+  constructor(public router: Router) {}
+
+  isRegisterRoute(): boolean {
+    return this.router.url === '/register';
+  }
 }
 

4de7ae8eca42b599c3c1c0b8a616e7b50a02ec17
nicolasArcamone97 <109641395+nicolasArcamone97@users.noreply.github.com>
2025-01-15 22:10:05 -0300

    eliminacion de guardas en archivo de rutas

--- src/app/models/tarea.ts
+++ src/app/models/tarea.ts
@@ -1,15 +1,8 @@
 import { ColorEnum } from "../enums/color.enum";
 
-export class Tarea {
+export interface Tarea {
     titulo: string
     descripcion: string;
-    color: ColorEnum
-    
-
-    constructor(titulo:string,descripcion:string,color:ColorEnum){
-        this.titulo = titulo,
-        this.descripcion = descripcion,
-        this.color = color
-    }   
+    color: ColorEnum 
 
 }
